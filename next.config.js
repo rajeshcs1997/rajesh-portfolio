954f380d4f97234a19e02f48c753e452dfee0543
Rajesh Prajapati <rajeshprajapati@Rajeshs-MacBook-Air.local>
2026-08-14 02:09:29 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 const path = require('path')
- 
+
 module.exports = {
+  output: 'export',
+
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
